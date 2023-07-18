3ea0e3a82b22409c8b1b919bf7c9b1c1462bb817
paulorenatoaz <paulorenatoaz@gmail.com>
2023-07-18 00:28:10 -0300

    refactor

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     packages=find_packages(),
     install_requires=[
         # List any dependencies your package requires
-        'numpy~=1.24.2',
-        'matplotlib~=3.7.1',
-        'shapely~=2.0.1',
-        'ipython==8.13.0',
-        'scikit-learn==1.3.0',
-        'scipy~=1.10.1',
-        'pygsheets~=2.0.6',
+        'numpy>=1.19.5',
+        'matplotlib>=3.3.3',
+        'shapely>=2.0.1',
+        'ipython>=8.12.2',
+        'scikit-learn>=1.3.0',
+        'scipy>=1.10.1',
+        'pygsheets>=2.0.6'
     ],
     author='Paulo Azevedo',
     author_email='paulorenatoaz@dcc.ufrj.br',
