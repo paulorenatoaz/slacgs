a5124c454738c9d576263b7dc53b32410a5f6d89
paulorenatoaz <paulorenatoaz@gmail.com>
2023-08-02 09:35:33 -0300

    Release 1.0

--- test/test.py
+++ test/test.py
@@ -1,7 +1,11 @@
 from slacgs.demo import *
 
 
-params = [1,1,2,3,4,-0.3,-0.2,-0.1,0.3,0,0,0.3,0,0.3,0.3]
+# params = [1,1,2,3,4,-0.3,-0.2,-0.1,0.3,0,0,0.3,0,0.3,0.3]
 
+params = [1,1,4, -0.4,0,0]
 
-run_custom_simulation_test(params, dims_to_compare=(2, 5))
+set_report_service_conf('academy', 'paulorenatoaz@gmail.com')
+
+# run_custom_simulation_test(params)
+run_experiment_simulation_test()
