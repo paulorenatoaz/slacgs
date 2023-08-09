c8dfdeef673b3003417dcf4f6c2d48bea781015e
paulorenatoaz <paulorenatoaz@gmail.com>
2023-08-09 18:49:46 -0300

    Release 1.0

--- slacgs/enumtypes.py
+++ slacgs/enumtypes.py
@@ -9,13 +9,14 @@ class LossType(Enum):
   - The hyperplane  :math:`h` defined by this equation represents the decision boundary that separates classes in a linearly separable space.
   - The hypothesis space :math:`H` consists of all possible linear classifiers.
 
-  Let :math:`\hat{h}^{(D)}` be the best empirical classifier in the dictionary :math:`H` for the dataset :math:`D` with :math:`d` features and :math:`n` samples, i.e., the classifier in :math:`H` with the minimum empirical error rate. Under these conditions, we can define three error rates:
+  Let :math:`\hat{h}^{(D)}` be the best empirical classifier in the dictionary :math:`H` for the dataset :math:`D` with :math:`d` features and :math:`n` samples, i.e., the classifier :math:`h` in :math:`H` with the minimum empirical error rate. Under these conditions, we can define three error rates:
     - :math:`L(\hat{h}^{(D)}) =` the theoretical error rate of the classifier :math:`\hat{h}^{(D)}`
     - :math:`\hat{L}(\hat{h}^{(D)}) =` the empirical error rate of the classifier :math:`\hat{h}^{(D)}`
     - :math:`\hat{L}(\hat{h}^{(D)},D') =` the empirical error rate of the classifier :math:`\hat{h}^{(D)}` using a test dataset :math:`D'`
 
   The Bayes Error (also: Bayes Risk) is defined as the minimum theoretical achievable error rate:
-    - :math:`\min_{h\in H} L(h) =` the smallest theoretically achievable error rate in :math:`H` classifying any Gaussian dataset :math:`D` with a given covariance matrix :math:`\Sigma`
+      - :math:`\min_{h\in H} L(h) =` the smallest theoretically achievable error rate for a classifier :math:`h` in dictionary :math:`H`.
+
   Attributes:
     - THEORETICAL: estimated using probability theory
     - EMPIRICALTRAIN: estimated using empirical approach with train data
